3406db16a0ecb07c4c12492cdfd0504c61e6fd94
mdeand <deandrade@posteo.com>
2025-12-10 14:21:46 -0500

    Update

--- flake.nix
+++ flake.nix
@@ -1,45 +1,97 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              (import inputs.rust-overlay)
-            ];
-          };
-
-          devShells.default =
-            let
-              toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-            in
-            pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                toolchain
-                openssl
-                protobuf
-                rust-analyzer # Used when running
-                pkg-config
-              ];
-            };
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
         };
-    };
+
+        naersk' = pkgs.callPackage naersk { };
+
+        buildInputs = with pkgs; [
+        ];
+
+        nativeBuildInputs =
+          with pkgs;
+          [
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "cargo"
+                "rustc"
+              ];
+            })
+
+            pkg-config
+            protobuf
+            openssl
+          ]
+          # TODO(mdeand): Add support for macOS 
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            alsa-lib
+          ];
+      in
+      rec {
+        defaultPackage = packages.engine;
+        packages = {
+          engine = naersk'.buildPackage {
+            src = ./.;
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+          };
+          container = pkgs.dockerTools.buildImage {
+            name = "engine";
+            config = {
+              entrypoint = [ "${packages.engine}/bin/engine" ];
+            };
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          RUST_SRC_PATH = "${
+            pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            }
+          }/lib/rustlib/src/rust/library";
+
+          nativeBuildInputs =
+            with pkgs;
+            [
+              nixfmt
+              cmake
+              rustc
+              rustfmt
+              cargo
+              clippy
+              rust-analyzer
+            ]
+            ++ buildInputs
+            ++ nativeBuildInputs;
+        };
+      }
+    );
 }
