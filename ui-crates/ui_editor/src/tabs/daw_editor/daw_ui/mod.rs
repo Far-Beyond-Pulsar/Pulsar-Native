b1694648c2049845fb7d79cc926b8a155501d277
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-12-21 16:51:46 -0500

    Update mod.rs

--- ui-crates/ui_editor/src/tabs/daw_editor/daw_ui/mod.rs
+++ ui-crates/ui_editor/src/tabs/daw_editor/daw_ui/mod.rs
@@ -8,13 +8,8 @@ mod mixer;
 // Individual panels - to be implemented one by one
 mod timeline;
 mod transport;
-// mod inspector;
 mod browser;
 mod toolbar;
 mod track_header;
-// mod clip_editor;
-// mod automation;
-// mod effects;
-// mod routing;
 
 pub use panel::DawPanel;
